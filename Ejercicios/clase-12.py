056fa871ea0f49923d21282a2eee43f57a6d0c78
perezmatias <maag.perez@alumno.um.edu.ar>
2023-06-07 14:19:52 -0300

    arreglado

--- Ejercicios/clase-12.py
+++ Ejercicios/clase-12.py
@@ -5,15 +5,14 @@ def calcular_termino(n, x):
     termino = ((-1) ** n) * (x ** (2 * n + 1)) / math.factorial(2 * n + 1)
     return termino
 
-def calcular_seno(x, num_terminos, resultado_compartido, lock):
+def calcular_seno(x, num_terminos, resultado_compartido):
     resultado_local = 0.0
     
     for n in range(num_terminos):
         termino = calcular_termino(n, x)
         resultado_local += termino
     
-    with lock:
-        resultado_compartido.append(resultado_local)
+    resultado_compartido.append(resultado_local)
 
 puntos = [
     {'cantidad_terminos': 12, 'x': 0.0, 'valor_referencia': 0.0},
@@ -22,8 +21,8 @@ puntos = [
     {'cantidad_terminos': 12, 'x': 3.141592653589793, 'valor_referencia': -1.7028581387855716e-13}
 ]
 
-lock = threading.Lock()
 hilos = []
+resultados = []  
 
 for punto in puntos:
     cantidad_terminos = punto['cantidad_terminos']
@@ -31,19 +30,20 @@ for punto in puntos:
     valor_referencia = punto['valor_referencia']
     resultado_compartido = []
 
-    for _ in range(cantidad_terminos):
-        hilo = threading.Thread(target=calcular_seno, args=(x, 1, resultado_compartido, lock))
-        hilo.start()
-        hilos.append(hilo)
+    hilo = threading.Thread(target=calcular_seno, args=(x, cantidad_terminos, resultado_compartido))
+    hilo.start()
+    hilos.append(hilo)
+    resultados.append(resultado_compartido)
 
-    for hilo in hilos:
-        hilo.join()
+for hilo in hilos:
+    hilo.join()
 
-    resultado = sum(resultado_compartido)
-    diferencia = resultado - valor_referencia
+for i, punto in enumerate(puntos):
+    resultado = sum(resultados[i])
+    diferencia = resultado - punto['valor_referencia']
 
-    print(f"Para x = {x}, cantidad de términos = {cantidad_terminos}:")
-    print(f"El desarrollo en serie de Taylor para sin({x}) es: {resultado}")
-    print(f"Valor de referencia: {valor_referencia}")
+    print(f"Para x = {punto['x']}, cantidad de términos = {punto['cantidad_terminos']}:")
+    print(f"El desarrollo en serie de Taylor para sin({punto['x']}) es: {resultado}")
+    print(f"Valor de referencia: {punto['valor_referencia']}")
     print(f"Diferencia: {diferencia}")
     print()
